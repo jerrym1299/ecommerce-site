bd51c3b42763ca1a9b61d76f6f5c3c683d8cd901
Manininjz <44416444+Manininjz@users.noreply.github.com>
2019-04-24 09:07:07 +1000

    Update connect.php
    FInal touch done i think

--- connect.php
+++ connect.php
@@ -33,6 +33,10 @@ else if (isset($_POST['name']) && isset($_POST['username1']) && isset($_POST['pa
     echo "Username already exists!";
     header("location: index.html");
   }
+  else {
+    echo "creation successful!"
+    mysql_query("INSERT INTO Users(name, username, password) VALUES ('$newusername', '$newuser', '$newuserpass')");
+  }
 }
 database_close()
 ?>
