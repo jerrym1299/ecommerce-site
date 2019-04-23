5a14f7071330e9cc9414cf663a5cf003191f4163
Glubs9 <37764604+Glubs9@users.noreply.github.com>
2019-04-23 16:53:47 +1000

    added checkLogin function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function checkLogin(usernameId, passwordId, serverUrl, outputId){
+    var username = document.getElementById(usernameId);
+    var password = document.getElementById(passwordId);
 
+    fetch (url, {headers: {'username': username, 'password': password}}).then(function (data){
+        document.getElementById(outputId).innerHTML = data;
+    });
+}
