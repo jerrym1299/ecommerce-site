4a2745f42d7176af91aa02445d09be46a5242867
Manininjz <44416444+Manininjz@users.noreply.github.com>
2019-04-23 17:32:03 +1000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,15 +1,3 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$databasename = "myDB";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-else (!$conn) {
-  
-}
+$database = mysqli('localhost', 'username', 'password');
 ?>
