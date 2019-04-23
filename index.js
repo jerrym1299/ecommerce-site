1ead06c26f7d8eea62439829def92a5f504debbf
Glubs9 <37764604+Glubs9@users.noreply.github.com>
2019-04-23 22:07:47 +1000

    deleted old fetch call, made file empty

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-function checkLogin(usernameId, passwordId, serverUrl, outputId){
-    var username = document.getElementById(usernameId);
-    var password = document.getElementById(passwordId);
 
-    fetch (url, {headers: {'username': username, 'password': password}}).then(function (data){
-        document.getElementById(outputId).innerHTML = data;
-    });
-}
