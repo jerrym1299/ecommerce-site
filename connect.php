d658b6926d224f6360d773101a1de7e253b9a53d
Manininjz <44416444+Manininjz@users.noreply.github.com>
2019-04-24 08:36:41 +1000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,10 @@
 <?php
-$database = mysqli('localhost', 'username', 'password');
+$host = "localhost" // plz sub in actual host
+$user = "username"// plz sub in actual db server username
+$password = "password" // plz sub in actual db server password
+$dbname = "database"// plz sub in actual db server db name
+$database = new mysqli($host', $user, $password, $dbname);
+if ($database->connect_errno) {
+  echo "Failed to connect: (" . $database->connect_errno . ") " . $database->connect_error;"
+}
 ?>
