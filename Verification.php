ff63252d548f448837a66e3a2cde7ade06cdf62e
Manininjz <44416444+Manininjz@users.noreply.github.com>
2019-04-24 09:17:02 +1000

    Update and rename connect.php to Verification.php

--- Verification.php
+++ Verification.php
@@ -1,16 +1,5 @@
 <?php
-$host = "localhost" // plz sub in actual host
-$user = "username"// plz sub in actual db server username
-$password = "password" // plz sub in actual db server password
-$dbname = "database"// plz sub in actual db server db name
-$database = new mysqli($host', $user, $password, $dbname);
-if ($database->connect_errno) {
-  echo "Failed to connect: (" . $database->connect_errno . ") " . $database->connect_error;"
-}
-else {
-  echo "connection successful";
-}
-
+include ("connection.php");
 if (isset($_POST['username2']) && isset($_POST['password2'])) {
   $user_name = $_POST['username2']
   $user_pass = $_POST['password2']
